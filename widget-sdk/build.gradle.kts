87913e19edca42dedfd9b0a14b66650f8ac08a6c
shivesh-lc <shivesh@limechat.ai>
2025-09-08 15:49:50 +0530

    Bump version to 0.0.8-beta

--- widget-sdk/build.gradle.kts
+++ widget-sdk/build.gradle.kts
@@ -64,7 +64,7 @@ afterEvaluate {
                 
                 groupId = "com.github.wavicle-limechat"
                 artifactId = "widget-sdk-android"
-                version = "0.0.7"
+                version = "0.0.8-beta"
                 
                 pom {
                     name.set("LimeChat Widget Android SDK")
