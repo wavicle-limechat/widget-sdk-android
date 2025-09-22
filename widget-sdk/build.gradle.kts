970e24b99084e82dc9fb37f758f71d58c61fce53
shivesh-lc <shivesh@limechat.ai>
2025-09-22 14:58:19 +0530

    feat: bump version to 0.0.9-beta-5
    - Update widget-sdk version to 0.0.9-beta-5
    - Update published SDK demo to use new version
    - Prepare for JitPack build with plug-and-play back button functionality

--- widget-sdk/build.gradle.kts
+++ widget-sdk/build.gradle.kts
@@ -64,7 +64,7 @@ afterEvaluate {
                 
                 groupId = "com.github.wavicle-limechat"
                 artifactId = "widget-sdk-android"
-                version = "0.0.9-beta-4"
+                version = "0.0.9-beta-5"
                 
                 pom {
                     name.set("LimeChat Widget Android SDK")
